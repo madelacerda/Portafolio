4646067fe2425eb189e2530f35ea3062d175ca91
madelacerda <53839267+madelacerda@users.noreply.github.com>
2021-07-07 04:35:53 -0400

    v2

--- src/sections/section2/Section2.js
+++ src/sections/section2/Section2.js
@@ -5,22 +5,23 @@ import FotoDescarga from "../../svg/file-download.svg";
 
 export const Section2 = () => {
   return (
-    <div style={{ padding: "80px ", width: "100%", paddingBottom: "250px" }}>
+    <div className="contenedor-general2">
       <div className="contenedor-imagen">
         <Foto />
+      </div>
+
+      <div className="contenedor-texto">
         <p className="titulo">Sobre mi</p>
-        <div className="contenedor-texto">
-          <p className="texto">
-            Desarrollador Full Stack, Con experiencia en lenguajes de
-            programación y plataformas web, con experiencia laboral tanto en
-            desarrollo de aplicaciones móviles y trabajando con metodologías
-            agiles tales como Scrum, trabajando siempre con equipos
-            multidisciplinarios de desarrollador
-          </p>
-          <div className="contenedor-cv">
-            <img src={FotoDescarga} className="foto" />
-            <p className="descargar">Descargar CV</p>
-          </div>
+        <p className="texto">
+          Desarrollador Full Stack, Con experiencia en lenguajes de programación
+          y plataformas web, con experiencia laboral tanto en desarrollo de
+          aplicaciones móviles y trabajando con metodologías agiles tales como
+          Scrum, trabajando siempre con equipos multidisciplinarios de
+          desarrollador
+        </p>
+        <div className="contenedor-cv">
+          <img src={FotoDescarga} className="foto" />
+          <p className="descargar">Descargar CV</p>
         </div>
       </div>
     </div>
--- src/sections/section4/Section4.js
+++ src/sections/section4/Section4.js
@@ -1,15 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import "./section4.css";
 import fotocal from "../../svg/undraw_feeling_proud_qne1.svg";
+import fotograd from "../../svg/graduation-cap.svg";
+import fototrabajos from "../../svg/suitcase.svg";
 const Section4 = () => {
+  const [show, setShow] = useState(true);
+
   return (
     <div className="contenedor4">
       <div className="contenedor-textos">
-        <p className="habilidades">Calificaciones</p>
-        <img src={fotocal} />
+        <div className="txt-titulo">
+          <p className="habilidades">Calificaciones</p>
+        </div>
+        <div className="contenedor-principal-calificaciones">
+          <div className="contenedor-estudios-trabajos">
+            <div className="cont-titulo-estudio" onClick={() => setShow(true)}>
+              <img src={fotograd} className="foto-grad" />
+              <p className="txt-trabajos">Estudios</p>
+            </div>
+            <div
+              className="cont-titulo-trabajos"
+              onClick={() => setShow(false)}
+            >
+              <img src={fototrabajos} className="foto-grad" />
+              <p className="txt-trabajos">Trabajos</p>
+            </div>
+          </div>
+          {show ? (
+            <div className="bottom">
+              {data.map((item, index) => (
+                <Item key={index} data={item} />
+              ))}
+              <div className="linea1"></div>
+              <div className="circulo1"></div>
+              <div className="circulo2"></div>
+              <div className="circulo3"></div>
+            </div>
+          ) : (
+            <div className="bottom">
+              {data2.map((item, index) => (
+                <Item1 key={index} data={item} />
+              ))}
+              <div className="linea2"></div>
+              <div className="circulo1"></div>
+              <div className="circulo2"></div>
+              {/* <div className="circulo3"></div> */}
+            </div>
+          )}
+        </div>
+
+        <div className="cont-right">
+          <img src={fotocal} className="foto-calificaciones" />
+        </div>
       </div>
     </div>
   );
 };
 
 export default Section4;
+
+const Item = ({ data }) => {
+  const { trabajo, fecha, clase } = data;
+  return (
+    <div className={`item ${clase}`}>
+      <div className="contenedor-estudios">
+        <p className="txt-establecimiento">{trabajo}</p>
+        <p className="txt-fecha">{fecha}</p>
+      </div>
+    </div>
+  );
+};
+
+const Item1 = ({ data }) => {
+  const { trabajo, fecha, clase } = data;
+  return (
+    <div className={`item ${clase}`}>
+      <div className="contenedor-estudios">
+        <p className="txt-trabajo">{trabajo}</p>
+        <p className="txt-fecha">{fecha}</p>
+      </div>
+    </div>
+  );
+};
+
+const data = [
+  {
+    clase: "elemento1",
+    trabajo: "Colegio Saint Johns School",
+    fecha: "Marzo 1999 - Diciembre 2013",
+  },
+  {
+    clase: "elemento2",
+    trabajo: "Ingenieria Comercial | Universidad del desarrollo ",
+    fecha: "Marzo 2014- Diciembre 2016",
+  },
+  {
+    clase: "elemento3",
+    trabajo: "Ingenieria Informatica | DuocUC",
+    fecha: "Marzo 2018- Diciembre 2021",
+  },
+];
+
+const data2 = [
+  {
+    clase: "elemento4",
+    trabajo: "Ingeniero Informatico | Inmobiliaria Integral",
+    fecha: "JUNIO – SEPTIEMBRE 2020",
+  },
+  {
+    clase: "elemento5",
+    trabajo: "Desarrollador Full Stack | Agencia Merken",
+    fecha: "Marzo 2014- Diciembre 2016",
+  },
+];
