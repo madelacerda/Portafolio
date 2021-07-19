f0622cd7c664a228e712dc594582c81d787178fa
madelacerda <53839267+madelacerda@users.noreply.github.com>
2021-07-19 16:26:27 -0400

    v4

--- src/sections/section6/Section6.js
+++ src/sections/section6/Section6.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./section6.css";
-import footer from "../../svg/Shiny.svg";
+import footer from "../../svg/Shiny.svg"
+import icongit from "../../svg/github.svg"
+import iconlin from "../../svg/linkedin.svg"
+import iconmail from "../../svg/at.svg"
+
 
 export const Section6 = () => {
   return (
     <div className="contenedor6">
       <div className="cont-footer">
         <img src={footer} className="img-footer" />
+        <img src={icongit} className="icon-footer" />
+      <img src={iconlin} className="icon-footer1" />
+      <img src={iconmail} className="icon-footer2" />
       </div>
-      <p>seccion6</p>
     </div>
   );
 };
